1613036e898fcbbf6db468b9753f16cd5ecfb2a0
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-05-26 21:47:45 +0700

    feat: modify org and adding icons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapp"
+    namespace = "dev.witheredflowers.enoughwithtodo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.myapp"
+        applicationId = "dev.witheredflowers.enoughwithtodo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapp
+package dev.witheredflowers.enoughwithtodo
 
 import io.flutter.embedding.android.FlutterActivity
 
